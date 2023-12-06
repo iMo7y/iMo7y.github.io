d610255564b6b2e0e42a8cc9a82b232a7ccd087a
Mohammed <saadisaadi768@gmail.com>
2023-12-06 10:56:43 -0600

    worked on tabs

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
 
-const Tab = () => {
+import state from '../store';
+
+const Tab = ( { tab, isFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Tab</div>
-  )
-}
+    <div
+    key={tab.name}
+    className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'} ${isActiveTab ? 'active' : ''}`}
+    onClick={handleClick}
+  >
+    
+  </div>
+);
+};
 
-export default Tab
+export default Tab;
