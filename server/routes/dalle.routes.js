d7d558cb6e8d80c29fa0c0c1a4fcd86d61cbe3f5
Mohammed <saadisaadi768@gmail.com>
2023-12-07 15:12:22 -0600

    worked on ai backend

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,16 @@
+//not working yet 
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAI } from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+
+
+router.route('/').get((req, res) => {
+    res.status(200).json({message: "Hello from DALL.E 2.0"})
+})
+
+export default router;
